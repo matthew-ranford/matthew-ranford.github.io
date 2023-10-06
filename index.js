d9812a4793160e2789bd35576b14ae909aa8883d
Matthew Ranford <matt.ranford16@gmail.com>
2023-10-07 12:52:51 +1300

    Added onclick function for main-image in about-section

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ $(document).ready(
     $('header').toggleClass('height')
   })
 )
+
+//// Onclick for about-section image ////
+
+let images = document.getElementsByClassName('main-image')
+
+function onClick() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = changePhoto
+  }
+}
+onClick()
+
+const photoCounts = {
+  click: 0,
+}
+
+function changePhoto(e) {
+  let mainPhoto = e.target
+
+  const id = mainPhoto.id
+
+  if (photoCounts[id] < 6) {
+    photoCounts[id]++
+    mainPhoto.src = `./images/about-section-image-change/about-section-${photoCounts[id]}.jpg`
+  } else if (photoCounts[id] === 6) {
+    photoCounts[id] = 0
+    mainPhoto.src = `./images/about-section-image-change/about-section-${photoCounts[id]}.jpg`
+  }
+}
